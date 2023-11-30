fa4c11d5437a82f915f4163d4c5cd4de2e6ff55e
Aman <you@example.com>
2023-12-01 00:57:30 +0530

    commit

--- connection.js
+++ connection.js
@@ -1,10 +1,10 @@
 const mysql=require('mysql');
 
 const conn=mysql.createConnection({
-    'host':'localhost',
-    'user':'root',
-    'password':'',
-    'database':'mystore'
+    host:process.env.DB_HOST,
+    user:process.env.DB_USERNAME,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_DBNAME
 })
 
 conn.connect((err)=>{
